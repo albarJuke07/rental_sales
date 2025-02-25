c36331da61e819127546f5e8121dc6fdd9c9d2a7
albarJuke07 <albar@jukesolutions.com>
2025-02-25 07:53:37 +0700

    task 1

--- models/rental_sales_product_template.py
+++ models/rental_sales_product_template.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class RentalSalesProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -10,3 +10,14 @@ class RentalSalesProductTemplate(models.Model):
     def _compute_unit_rented(self):
       for rec in self:
         rec.count_rent = 0
+        
+    def action_rental_products(self):
+      return {
+          "name": _("Rental Product"),
+          "type": "ir.actions.act_window",
+          "view_mode": "tree,form",
+          "res_model": "product.template",
+          "target": "current",
+          "domain": [("is_rent", "=", True)],
+          "context": {"default_is_rent": self.id}
+      }
